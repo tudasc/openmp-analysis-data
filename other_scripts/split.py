e7bf579761ae1cd8a70d8abce0aee7b6841af50f
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-04-16 14:51:48 +0200

    added script used to sort and split

---
+++ other_scripts/split.py
@@ -0,0 +1,23 @@
+#!/bin/env/python
+
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv('found_repos.csv', index_col=0)
+    df = df.sort_values('Stars', ascending=False)
+
+    df_tj = df.iloc[0::4]
+    df_ci = df.iloc[1::4]
+    df_rs = df.iloc[2::4]
+    df_mg = df.iloc[3::4]
+    
+    df_tj.to_csv('tj.csv')
+    df_ci.to_csv('ci.csv')
+    df_rs.to_csv('rs.csv')
+    df_mg.to_csv('mg.csv')
+
+
+
+if __name__ == '__main__':
+    main()
