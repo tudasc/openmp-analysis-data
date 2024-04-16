099bdcf37d85e99dfcce83885b95e8719c535bbd
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-04-16 15:49:37 +0200

    added capturing of git commit to repo download script

--- other_scripts/download_repos.py
+++ other_scripts/download_repos.py
@@ -8,30 +8,49 @@ from tqdm import tqdm
 REPO_PATH = "./REPOS/"
 
 
-def cloneRepo(repoUrl, path, re_download=False):
+def cloneRepo(repoUrl, path, commit_hash=None):
     try:
         # remove any old repo
-        if os.path.isdir(path):
-            if re_download:
-                subprocess.check_output(f'rm -rf {path}', stderr=subprocess.STDOUT, shell=True)
-            else:
-                # already present
-                return
+        if not os.path.isdir(path):
+            # not already present:
+            # download
+            subprocess.check_output(f'git clone --depth 1 {repoUrl} {path}', stderr=subprocess.STDOUT, shell=True)
 
-        # download
-        subprocess.check_output(f'git clone --depth 1 {repoUrl} {path}', stderr=subprocess.STDOUT, shell=True)
-
+        # get current hash, remove trailing \n
+        current_hash = subprocess.check_output(f'git rev-parse --verify HEAD', cwd=path, stderr=subprocess.STDOUT,
+                                               shell=True, encoding='UTF-8').strip()
+        if commit_hash is None or current_hash == commit_hash:
+            return current_hash
+        else:
+            # fetch different revision
+            # TODO one could check that origin-url is set up correctly
+            subprocess.check_output(f'git fetch --depth 1 origin {commit_hash}', cwd=path, stderr=subprocess.STDOUT,
+                                    shell=True)
+            subprocess.check_output(f'git checkout {commit_hash}', cwd=path, stderr=subprocess.STDOUT,
+                                    shell=True, )
+            return commit_hash
 
     except subprocess.CalledProcessError as e:
         print("ERROR: downloading Repo:")
         print(e.output)
 
 
-def main():
-    df = pd.read_csv('tj.csv', index_col=0)
+def apply_dowload_repo(row):
+    print(row)
+    h = None
+    if pd.notna(row['usedHash']):
+        h = row['usedHash']
+    return cloneRepo(row["URL"], REPO_PATH + row["Code"].replace('/', '--'), h)
 
-    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        cloneRepo(row["URL"], REPO_PATH + row["Code"].replace('/', '--'))
+
+def main():
+    df_full = pd.read_csv('tj.csv', index_col=0)
+
+    df = df_full.iloc[0:3]
+    df['usedHash'] = df.apply(apply_dowload_repo, axis=1)
+    df['expert'] = "tj"
+
+    df.to_csv('tj_result.csv')
 
 
 if __name__ == '__main__':
