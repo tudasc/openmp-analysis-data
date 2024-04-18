27996f6bbad53a6bd0c66840f6b8e2afa0e4a2ec
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-04-18 10:15:57 +0200

    reworked script to try out given build script when applicable

--- other_scripts/download_repos.py
+++ other_scripts/download_repos.py
@@ -47,23 +47,17 @@ def apply_dowload_repo(row):
 
 
 def main():
-    df_full = pd.read_csv('tj.csv', index_col=0)
+    df_full = pd.read_csv('tj_result.csv', index_col=0)
 
     # df = df_full.iloc[0:2]
     df = df_full
 
     df = df.progress_apply(one_repo_at_a_time, axis=1)
 
-    # df['usedHash'] = df.progress_apply(apply_dowload_repo, axis=1)
     # df['expert'] = "tj"
 
-    # df.to_csv('tj_result.csv')
+    print(df['use_configure'].value_counts())
 
-    # df['use_cmake'] = df.progress_apply(has_cmake_list, axis=1)
-
-    # print(df['use_cmake'].value_counts())
-
-    # df['DEFAULT_CMAKE_BUILDABLE'] = df.progress_apply(can_build_default_cmake, axis=1)
     # print(df['DEFAULT_CMAKE_BUILDABLE'].value_counts())
 
     df.to_csv('tj_result.csv')
@@ -71,52 +65,34 @@ def main():
 
 def one_repo_at_a_time(row):
     path = REPO_PATH + row["Code"].replace('/', '--')
+    if row['use_cmake']:
+        return row
+
     row['usedHash'] = apply_dowload_repo(row)
-    if "CMakeLists.txt" in os.listdir(path):
-        row['use_cmake'] = True
-        if can_build_default_cmake(row):
-            row['DEFAULT_CMAKE_BUILDABLE'] = True
+    if "configure" in os.listdir(path):
+        row['use_configure'] = True
+        if try_build_script(path, "./scripts/default_configure.sh"):
+            row['build_script'] = "default_configure.sh"
         else:
-            row['DEFAULT_CMAKE_BUILDABLE'] = False
+            pass
+            # not buildable
     else:
-        row['use_cmake'] = False
-        row['DEFAULT_CMAKE_BUILDABLE'] = False
+        row['use_configure'] = False
     # save storage space
     shutil.rmtree(path)
     return row
 
 
-def has_cmake_list(row):
-    path = REPO_PATH + row["Code"].replace('/', '--')
-    if "CMakeLists.txt" in os.listdir(path):
-        return True
-    return False
-
-
-def can_build_default_cmake(row):
-    path = REPO_PATH + row["Code"].replace('/', '--')
-    if "CMakeLists.txt" in os.listdir(path):
-        if try_default_cmake_build(path):
-            return True
-    return False
-
-
-def try_default_cmake_build(path):
-    build_dir = path + "/build"
+def try_build_script(path, script):
+    assert os.path.isfile(script)
     try:
-        # remove
-        if os.path.isdir(build_dir):
-            shutil.rmtree(build_dir)
-        os.makedirs(build_dir, exist_ok=True)
-        # configure
-        subprocess.check_output(f'cmake .. -DCMAKE_C_FLAGS="-O0" -DCMAKE_CXX_FLAGS="-O0"', cwd=build_dir,
-                                stderr=subprocess.STDOUT,
-                                shell=True, encoding='UTF-8')
-        # building
-        subprocess.check_output(f'cmake --build .', cwd=build_dir,
-                                stderr=subprocess.STDOUT,
-                                shell=True, encoding='UTF-8')
-        return True
+        output = subprocess.check_output(script, cwd=path,
+                                         stderr=subprocess.STDOUT,
+                                         shell=True, encoding='UTF-8')
+        if "BUILD SUCCESSFUL" in output:
+            return True
+        else:
+            return False
 
     except subprocess.CalledProcessError as e:
         print("ERROR: building Repo:")
